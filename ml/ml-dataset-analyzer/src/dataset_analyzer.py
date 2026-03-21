16f7081b843adea0cba8d02718d10b24e6959a1c
Ranjan Ali <ranjan12nov90@gmail.com>
2026-03-21 23:04:04 +0530

    [main]: model is updated, ml data analyzer class is updated

--- ml/ml-dataset-analyzer/src/dataset_analyzer.py
+++ ml/ml-dataset-analyzer/src/dataset_analyzer.py
@@ -2,15 +2,21 @@
 #-----This class load the dataset and perform basic analysis-----
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class DataSetAnalyzer:
     def __init__(self,filepath):
         self.filepath = filepath
         self.df = None
+        self.X = None
+        self.y = None
+        self.model = None
 
     def load_data(self):
         self.df = pd.read_csv(self.filepath)
         print("Data loaded successfuly")
+        return self.df
 
     def basic_info(self):
         print("Shape:", self.df.shape)
@@ -30,9 +36,62 @@ class DataSetAnalyzer:
         self.df.hist(figsize = (10,8))
         plt.show() 
 
+    def prepare_data(self, target_col):
+        """
+        Prepares the dataset for modeling by splitting it into features (X) and target (y).
+
+        Parameters:
+        target_col (str): The name of the target column in the dataset.
+        """
+        if self.df is None:
+            print("Data is not loaded")
+            return
+        elif target_col not in self.df.columns :
+            print(f"{target_col} not found in the dataset")
+            return
+        else:
+            self.X = self.df.drop(target_col, axis = 1)
+            self.y = self.df[target_col]
+            print("\nData prepared successfully")
+
+    def train_model(self,model):
+        if self.X is None or self.y is None:
+            print("\nData is not prepared")
+            return
+        else:
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y, test_size= 0.2, random_state=42)
+
+        self.model = model
+        self.model.fit(self.X_train,self.y_train)
+        print("Model is trained successfully")
+
+    def evaluate_model(self):
+        if self.model is None:
+            print("model is not trained")
+            return
+        else:
+             predictions = self.model.predict(self.X_test)
+             accuracy = accuracy_score(self.y_test,predictions)
+        
+        return accuracy     
+
+    def predict(self, input_data):
+        if self.model is None:
+            print("Model is not trained")
+        else:
+            input_df = pd.DataFrame([input_data], columns=self.X.columns)
+            prediction = self.model.predict(input_df)  
+
+        return prediction[0]     
+
+
+
+
+
     def run_all(self):
         self.load_data()
         self.basic_info()
         self.statistics()
         self.visualization()
+
 
